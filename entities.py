8845cf0fe4a4d3e0b40516f4595deb307c6a8667
sterco_raro <marco.gargaro23@gmail.com>
2022-08-07 22:46:09 +0200

    Pulizie di primavera e documentazione basilare

--- entities.py
+++ entities.py
@@ -25,15 +25,20 @@ except ImportError as importErr:
 
 
 class Entity(pygame.sprite.Sprite):
-	"""Generic entity
+	"""Generic entity with a sprite, movement and flipping mechanics
 	Returns: object
-	Attributes: position_xy (starting position), file_name (sprite file name)"""
+	Attributes:
+		image (pygame.Surface),
+		rect (pygame.Rect),
+		speed (int),
+		position (pygame.Vector2),
+		flipped (bool)"""
 	def __init__(self, position_xy, file_name, speed=32):
 		pygame.sprite.Sprite.__init__(self)
 		# Sprite
 		self.image = load_image(file_name)
 		self.rect = self.image.get_rect()
-		# Update sprite rect position
+		# Initialize sprite rect position
 		self.rect.center = position_xy
 		# Movement
 		self.speed = speed
@@ -50,26 +55,6 @@ class Entity(pygame.sprite.Sprite):
 			self.image = pygame.transform.flip(self.image, True, False)
 			self.flipped = False
 
-	def check_collisions_point(self, position_xy, collisions):
-		"""Check for a collision between this entity's rect and collisions rects list"""
-		for rect in collisions:
-			if rect.collidepoint(position_xy):
-				return True
-		return False
-
-	def check_collisions_rect(self, rect):
-		"""Check for a collision between this entity's rect and collisions rects list"""
-		if rect.colliderect(self.rect):
-			return True
-		return False
-
-	def check_collisions_rects(self, collisions):
-		"""Check for a collision between this entity's rect and collisions rects list"""
-		for rect in collisions:
-			if rect.colliderect(self.rect):
-				return True
-		return False
-
 	def clamp_to_map(self, position_xy):
 		"""Clamp position_xy vector to world map area, returns the clamped vector"""
 		x = min(WORLD_WIDTH - self.rect.width/2, max(0, position_xy[0]))
@@ -84,8 +69,9 @@ class Entity(pygame.sprite.Sprite):
 		# Calculate new position
 		new_position = self.position + direction * dt * self.speed/DAMPING_FACTOR
 		# Check for possible collisions
-		if self.check_collisions_point(new_position, collisions):
-			return
+		for rect in collisions:
+			if rect.collidepoint(new_position):
+				return
 		# No collisions, update entity position
 		self.position = new_position
 		self.rect.center = self.position
@@ -97,10 +83,14 @@ class Entity(pygame.sprite.Sprite):
 
 
 class Player(Entity):
-	"""Player entity
-	Returns: player object
-	Functions: update
-	Attributes: /"""
+	"""Player entity, with combat component and manual equipment slots
+	Returns: object
+	Attributes:
+		combat (CombatSystem)
+		weapon_slot_left (Weapon)
+		weapon_slot_right (Weapon)
+		cursor (Cursor)
+		camera (SimpleCamera"""
 	def __init__(
 		self,
 		position_xy, file_name="fatso.png", speed=32,	# Entity attributes
@@ -118,23 +108,22 @@ class Player(Entity):
 		self.camera = SimpleCamera(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
 
 	def render(self, surface, show_collision_rects):
+		"""Draw the player sprite, weapons and crosshair on the given surface"""
 		if not surface: return
 		# Draw player weapons
 		surface.blit(self.weapon_slot_left.image, self.weapon_slot_left.rect)
 		surface.blit(self.weapon_slot_right.image, self.weapon_slot_right.rect)
 		# Draw player sprite
 		surface.blit(self.image, self.rect)
-
-		# TODO TMP DEBUG
 		# Draw collision layer for each attacking hand
 		if show_collision_rects:
 			pygame.draw.rect(surface, DBG_COLLISION_PLAYER, self.weapon_slot_left.rect, width=1)
 			pygame.draw.rect(surface, DBG_COLLISION_PLAYER, self.weapon_slot_right.rect, width=1)
-
 		# Draw crosshair
 		surface.blit(self.cursor.image, self.cursor.rect)
 
 	def update(self, dt, collisions, entities):
+		"""Update entity logic"""
 		# Mouse buttons state
 		mouse_left 		= None
 		mouse_middle 	= None
@@ -148,7 +137,7 @@ class Player(Entity):
 		if pressed[pygame.K_a] or pressed[pygame.K_LEFT]: 	direction += (-1,  0)
 		if pressed[pygame.K_s] or pressed[pygame.K_DOWN]: 	direction += ( 0,  1)
 		if pressed[pygame.K_d] or pressed[pygame.K_RIGHT]: 	direction += ( 1,  0)
-		# if pressed click sinistro: attacca col sinistro, ecc
+		# React to mouse clicks (attacks)
 		mouse_left, mouse_middle, mouse_right = pygame.mouse.get_pressed()
 		if mouse_left: self.attack_with_slot(True, entities)
 		if mouse_right: self.attack_with_slot(False, entities)
@@ -163,6 +152,7 @@ class Player(Entity):
 		self.camera.update(self.rect)
 
 	def attack_with_slot(self, left_slot, entities):
+		"""Try to attack a colliding entity using either the left or right slot"""
 		# Check collisions between attack rect and entities
 		for entity in entities:
 			# Deal damage to colliding entities with the correct slot (using a slightly bigger rect)
@@ -178,7 +168,14 @@ class Player(Entity):
 
 
 class Enemy(Entity):
-	"""TODO docstring for Enemy"""
+	"""Enemy entity, with combat and basic AI component
+	Returns: object
+	Attributes:
+		combat (CombatSystem)
+		follower (FollowerAI)
+		attacking (bool)
+		ev_attack_cooldown(int, pygame event id)
+		corpse (pygame.Surface)"""
 	def __init__(
 		self,
 		position_xy, file_name="geezer_1.png", speed=28,			# Entity attributes
@@ -196,6 +193,7 @@ class Enemy(Entity):
 		self.corpse = load_image("corpse_generic.png")
 
 	def render(self, surface, dead_surface, show_collision_rects):
+		"""Draw enemy sprite or corpse. Returns True when enemy is dead"""
 		# Exit early when surface is None
 		if not surface: return
 		# Render corpse to different surface when entity is dead
@@ -210,24 +208,23 @@ class Enemy(Entity):
 		return False
 
 	def update(self, events, dt, collisions, player):
-
+		"""Update entity logic"""
 		if not self.combat.is_alive():
 			return
 		if not self.follower:
 			return
-
+		# Catch timer ending event for attack cooldown
 		for event in events:
 			if event.type == self.ev_attack_cooldown:
 				self.attacking = False
-
 		# Check collision with player entity to attack (and pause follower ai)
-		if not self.attacking and self.check_collisions_rect(player.rect):
+		if not self.attacking and self.rect.colliderect(player.rect):
+			# Change state
 			self.attacking = True
 			# Restart attack cooldown
 			pygame.time.set_timer(self.ev_attack_cooldown, 1000, loops=1)
 			# Apply attack and stop following the player
 			self.combat.attack_target(player)
-
 		# Move entity towards player while not colliding
 		self.follower.update(dt, collisions)
 
@@ -236,33 +233,38 @@ class Enemy(Entity):
 
 
 class Weapon(Entity):
-	"""Weapon entity 
-	Returns: weapon object
-	Functions: update
-	Attributes: position_xy, parent, orbit_distance, target, file_name"""
+	"""Weapon entity, rotating around the parent
+	Returns: object
+	Attributes:
+		parent (Entity)
+		orbit_distance (int)
+		original_image (pygame.Surface, image copy used in rotation transforms"""
 	def __init__(self, position_xy, parent, file_name="weap_hand_L.png", orbit_distance=20, damage=1):
 		super().__init__(position_xy, file_name)
-
-		self.parent = parent						# who is using this weapon?
-		self.orbit_distance = orbit_distance		# how far should the weapon orbit around the parent
-
-		if self.orbit_distance < 0:										# if weapon is on opposite side of parent
-			self.image = pygame.transform.flip(self.image, False, True)	# flip the image
-		self.original_image = self.image								# save a copy of this image for rendering rotations
-
-		self.damage = damage
+		# Parent entity reference
+		self.parent = parent
+		# Orbit position offset from parent sprite
+		self.orbit_distance = orbit_distance
+		# Flip image if this weapon is on the left side of the parent
+		if self.orbit_distance < 0:
+			self.image = pygame.transform.flip(self.image, False, True)
+		# Keep a copy of the original image for rotations
+		self.original_image = self.image
 
 	def update(self, target):
+		"""Update logic based on parent and target position"""
+		# Default stance
 		if target is None:
 			lookdir = pygame.Vector2(-1, 0)
+		# Get vector pointing at current target
 		else:
 			lookdir = target.rect.center - self.parent.position
 			lookdir = lookdir.rotate(90)
 			lookdir.normalize_ip()
-
+		# Calculate position with offset and angle to update rotation
 		weapon_position = lookdir * self.orbit_distance
 		rotation_angle = lookdir.angle_to((0, 1))
-
+		# Update position and rotate sprite
 		self.rect.center = self.parent.rect.center + weapon_position
 		self.image = pygame.transform.rotate(self.original_image, rotation_angle)
 
@@ -273,13 +275,12 @@ class Weapon(Entity):
 class Cursor(Entity):
 	"""Cursor entity that follows mouse movement
 	Returns: object
-	Functions: update
-	Attributes: none"""
+	Attributes: None"""
 	def __init__(self, position_xy, file_name="cursor_crosshair.png"):
 		super().__init__(position_xy, file_name)
 
 	def update(self, camera_position):
+		"""Update position based on cursor and viewport"""
 		cursor_position = pygame.mouse.get_pos()
-		# Update cursor position with viewport coordinates
 		self.rect.center = pygame.Vector2(	cursor_position[0] + camera_position[0],
 											cursor_position[1] + camera_position[1])
