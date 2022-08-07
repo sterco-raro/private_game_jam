b17a0e4a549db98c0ee2bfb1c626c9ef5f6ddc55
sterco_raro <marco.gargaro23@gmail.com>
2022-08-07 20:30:09 +0200

    Pulizia e sistemazioni varie per la mappa

--- game_map.py
+++ game_map.py
@@ -3,10 +3,8 @@
 
 try:
 	import sys
-	import random
 	import pygame
 	import numpy as np
-	from enum import auto, Enum
 	from constants import TILE_SIZE, TILES_INFO, DBG_COLLISION_TILES
 	from utils import load_image
 except ImportError as importErr:
@@ -17,60 +15,58 @@ except ImportError as importErr:
 # -------------------------------------------------------------------------------------------------
 
 
-class Tileset(object):
-	"""Load and holds a tileset from a png image"""
-	def __init__(self, file_name, size=(TILE_SIZE, TILE_SIZE), margin=1, spacing=1):
-		self.file_name = file_name
-		self.size = size
-		self.margin = margin
-		self.spacing = spacing
-
-		self.image = load_image(file_name)
-		self.rect = self.image.get_rect()
-
-		self.tiles = []
-		self.load()
-
-	def load(self):
-		self.tiles = []
-
-		tile = None
-		x0 = y0 = self.margin
-		w, h = self.rect.size
-		dx = self.size[0] + self.spacing
-		dy = self.size[1] + self.spacing
-
-		for x in range(x0, w, dx):
-			for y in range(y0, h, dy):
-				tile = pygame.Surface(self.size)
-				tile.blit(self.image, (0, 0), (x, y, *self.size))
-				self.tiles.append(tile)
-
-
-# -------------------------------------------------------------------------------------------------
-
-
 class Tilemap(object):
-	"""TODO docstring for Tilemap"""
-	def __init__(self, tileset, size=(10, 20), file_name=None):
-		self.size = size
-		self.tileset = tileset
+	"""Map handler working with square tiles of TILE_SIZE pixels
+	Returns: object
+	Attributes:
+		map (numpy array),
+		size (tuple (int, int)),
+		collision_map (pygame.Rects list),
+		tile_floor (pygame.Surface),
+		tile_wall (pygame.Surface),
+		file_name (str, relative path)"""
+	def __init__(self, size=(10, 20), file_name=None):
 		self.map = None
+		self.size = size
 		self.collision_map = []
-
+		self.file_name = file_name
+		# Tileset
 		self.tile_floor = load_image("floor_1.png")
 		self.tile_wall = load_image("wall_1.png")
-
-		if file_name:
+		# Load map on start when available
+		if self.file_name:
 			self.set_from_file(file_name)
 
-	def rebuild_collision_map(self, data):
-		# Exit early when data is empty
-		if len(data) == 0: return
+	def render_tile(self, surface, tile, position, show_collision):
+		"""Draw a tile on a surface with given position and optional collision rect"""
+		# Draw current tile on surface
+		surface.blit(tile, position)
+		# Draw collision layer
+		if show_collision:
+			pygame.draw.rect(surface, DBG_COLLISION_TILES, pygame.Rect(position, (TILE_SIZE, TILE_SIZE)), width=1)
 
+	def render(self, surface, show_collision_rects):
+		"""Draw the whole world map on the given surface. Can draw collisions layer when needed"""
+		x = 0
+		y = 0
+		n = self.size[0]
+		m = self.size[1]
+		for i in range(n):
+			for j in range(m):
+				# Position in pixels
+				x = j * TILE_SIZE
+				y = i * TILE_SIZE
+				# Draw correct map tile
+				if self.map[i, j] == 0:
+					self.render_tile(surface, self.tile_floor, (x, y), False)
+				else:
+					self.render_tile(surface, self.tile_wall, (x, y), show_collision_rects)
+
+	def rebuild_collision_map(self, data):
+		"""Build a new map of collidable tiles"""
+		if len(data) == 0: return
 		# Reset collisions map
 		self.collision_map = []
-
 		# Build collisions map, one pygame rect for each non-walkable tile
 		n = len(data[0])
 		m = len(data)
@@ -79,40 +75,25 @@ class Tilemap(object):
 				if data[y][x] not in TILES_INFO["walkable"]:
 					self.collision_map.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
-	def render(self, surface, show_collision_rects):
-		tile = None
-		n = self.size[0]
-		m = self.size[1]
-		for i in range(n):
-			for j in range(m):
-				# tile = self.tileset.tiles[self.map[i, j]]
-				# Choose correct map tile
-				if self.map[i, j] == 0:
-					tile = self.tile_floor
-				else:
-					tile = self.tile_wall
-				# Draw current tile on surface
-				surface.blit(tile, (j * TILE_SIZE, i * TILE_SIZE))
-				# Draw collision layer when necessary
-				if show_collision_rects:
-					pygame.draw.rect(surface, DBG_COLLISION_TILES, tile.get_rect(), width=1)
-
 	def set_zero(self):
+		"""Reset world map with zeros"""
 		self.map = np.zeros(self.size, dtype=int)
 
-	def set_random(self):
-		self.map = np.random.randint(len(self.tileset.tiles), size=self.size)
-
 	def set_from_file(self, file_name):
+		"""Load world map from disk"""
 		data = []
+		# Read map file into data
 		with open(file_name, "r") as fin:
 			for line in fin:
 				data.append([ int(x) for x in line.split() ])
+		# Build numpy array from file
 		self.map = np.array(data, dtype=int)
+		# Update collisions map
 		self.rebuild_collision_map(data)
 
 	def save_to_file(self):
-		with open("data/level.txt", "w") as fout:
+		"""Store current world map on disk, appending .save to the original file name"""
+		with open("{}.save".format(self.file_name), "w") as fout:
 			for x in range(self.size[0]):
 				for y in range(self.size[1]):
 					fout.write(str(self.map[x, y]) + " ")
