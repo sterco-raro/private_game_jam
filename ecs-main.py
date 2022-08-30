8c0650435264c8faf1acbf22373f680067b60ac4
sterco_raro <marco.gargaro23@gmail.com>
2022-08-30 14:25:31 +0200

    Added basic weapons functionalities. All level initializations are now made in a different function

--- ecs-main.py
+++ ecs-main.py
@@ -8,6 +8,9 @@
 #
 
 
+# TODO BUG movement is slower when direction is positive
+
+
 try:
 	import sys
 	import random
@@ -36,16 +39,18 @@ class Velocity(object):
 	damping_factor: int = DAMPING_FACTOR
 
 
-@dataclass
-class Renderable:
+@dataclass()
+class BasicSprite:
 	file_name: str = "fatso.png"
 	starting_position: tuple = (0, 0)
-	flippable: bool = True
+	flippable: bool = False
 	flipped: bool = False
 	is_moving_left: bool = False
 
 	def __post_init__(self):
 		self.image = load_image(self.file_name)
+		if self.flipped:
+			self.image = pygame.transform.flip(self.image, False, True)
 		self.rect = self.image.get_rect()
 		self.rect.center = self.starting_position
 
@@ -59,21 +64,34 @@ class PlayerController:
 class SimpleCamera:
 	width: int
 	height: int
-	target: Renderable
+	target: BasicSprite
 
 	def __post_init__(self):
 		self.rect = pygame.Rect(0, 0, self.width, self.height)
+
+
+@dataclass
+class Equippable:
+	parent: int
+	original_image: pygame.Surface
+	offset_x: int = 0
+	offset_y: int = 0
+
+
+@dataclass
+class Target:
+	current: int = -1
 
 
 # -------------------------------------------------------------------------------------------------
 
 
 class RenderSystem(esper.Processor):
-	def __init__(self, canvas, background, viewport, camera):
+	def __init__(self, canvas, background, screen, camera):
 		super().__init__()
 		self.canvas = canvas
 		self.background = background
-		self.viewport = viewport
+		self.screen = screen
 		self.camera = camera
 
 	def flip_sprite(self, sprite):
@@ -88,12 +106,12 @@ class RenderSystem(esper.Processor):
 		# clear
 		self.canvas.blit(self.background, (0, 0))
 		# render all to canvas
-		for ent, sprite in self.world.get_component(Renderable):
+		for ent, sprite in self.world.get_component(BasicSprite):
 			if sprite.flippable:
 				self.flip_sprite(sprite)
 			self.canvas.blit(sprite.image, sprite.rect)
-		# render to viewport
-		self.viewport.blit(self.canvas, (0, 0), self.camera.rect)
+		# render to screen
+		self.screen.blit(self.canvas, (0, 0), self.camera.rect)
 		# update display
 		pygame.display.update()
 
@@ -123,7 +141,7 @@ class MovementSystem(esper.Processor):
 		self.max_y = max_y
 
 	def process(self, dt):
-		for ent, (vel, sprite) in self.world.get_components(Velocity, Renderable):
+		for ent, (vel, sprite) in self.world.get_components(Velocity, BasicSprite):
 			# normalize direction
 			if vel.direction.length() > 0:
 				vel.direction.normalize_ip()
@@ -141,26 +159,6 @@ class MovementSystem(esper.Processor):
 			sprite.is_moving_left = vel.direction[0] < 0
 
 
-class ViewportSystem(esper.Processor):
-
-	def __init__(self, max_width, max_height):
-		super().__init__()
-		self.max_width = max_width
-		self.max_height = max_height
-
-	def process(self, dt):
-		for ent, (cam, sprite) in self.world.get_components(SimpleCamera, Renderable):
-			# center
-			x = cam.target.rect.centerx - cam.width // 2
-			y = cam.target.rect.centery - cam.height // 2
-			# clamp
-			x = min(self.max_width - cam.width, max(0, x))
-			y = min(self.max_height - cam.height, max(0, y))
-			# update position
-			cam.rect.x = x
-			cam.rect.y = y
-
-
 class CrosshairSystem(esper.Processor):
 
 	def __init__(self, crosshair, camera_target):
@@ -172,50 +170,134 @@ class CrosshairSystem(esper.Processor):
 		# do nothing when cursor is absent
 		if not self.crosshair: return
 		# get cursor and camera
-		cursor = self.world.component_for_entity(self.crosshair, Renderable)
+		cursor = self.world.component_for_entity(self.crosshair, BasicSprite)
 		camera = self.world.component_for_entity(self.camera_target, SimpleCamera)
 		# get mouse position
 		position = pygame.mouse.get_pos()
 		# update cursor position based on camera viewport
 		cursor.rect.center = pygame.Vector2(position[0] + camera.rect.x, position[1] + camera.rect.y)
 
+
+class WeaponSystem(esper.Processor):
+
+	def __init__(self):
+		super().__init__()
+
+	def process(self, dt):
+		for ent, (sprite, equip, target) in self.world.get_components(BasicSprite, Equippable, Target):
+			# Get parent entity sprite
+			parent_sprite = self.world.component_for_entity(equip.parent, BasicSprite)
+			# Default stance
+			if target.current == -1:
+				lookdir = pygame.Vector2(-1, 0)
+			# Get vector pointing at current target
+			else:
+				target_sprite = self.world.component_for_entity(target.current, BasicSprite)
+				lookdir = pygame.Vector2(target_sprite.rect.x - parent_sprite.rect.x, target_sprite.rect.y - parent_sprite.rect.y)
+				lookdir = lookdir.rotate(90)
+				lookdir.normalize_ip()
+			# Get current rotation angle based on lookdir
+			rotation_angle = lookdir.angle_to((0, 1))
+			# Apply Equippable offset
+			sprite.rect.center = (	parent_sprite.rect.center[0] + equip.offset_x,
+									parent_sprite.rect.center[1] + equip.offset_y)
+			# Rotate sprite
+			sprite.image = pygame.transform.rotate(equip.original_image, rotation_angle)
+
+
+class ViewportSystem(esper.Processor):
+
+	def __init__(self, max_width, max_height):
+		super().__init__()
+		self.max_width = max_width
+		self.max_height = max_height
+
+	def process(self, dt):
+		for ent, (cam, sprite) in self.world.get_components(SimpleCamera, BasicSprite):
+			# center
+			x = cam.target.rect.centerx - cam.width // 2
+			y = cam.target.rect.centery - cam.height // 2
+			# clamp
+			x = min(self.max_width - cam.width, max(0, x))
+			y = min(self.max_height - cam.height, max(0, y))
+			# update position
+			cam.rect.x = x
+			cam.rect.y = y
+
+
+# -------------------------------------------------------------------------------------------------
+
+
+def create_level_arena():
+	"""TODO docstring for create_level_arena"""
+	# Screen viewport
+	viewport = pygame.display.set_mode(SCREEN_SIZE.size)
+	# Window title
+	pygame.display.set_caption("POSTE ITALIANE")
+	# Drawing surfaces
+	canvas = pygame.Surface((WORLD_WIDTH, WORLD_HEIGHT)).convert()
+	background = pygame.Surface((WORLD_WIDTH, WORLD_HEIGHT)).convert()
+	# Background map
+	tilemap = Tilemap(size=(MAP_WIDTH, MAP_HEIGHT), file_name=LEVEL_ARENA)
+	tilemap.render(background, False)
+	# ECS container object
+	world = esper.World()
+	# Mouse cursor entity
+	crosshair = world.create_entity()
+	world.add_component(crosshair, BasicSprite(file_name="cursor_crosshair.png"))
+	# Player entity
+	player = world.create_entity()
+	player_x = WORLD_WIDTH//2 - TILE_SIZE//2
+	player_y = WORLD_HEIGHT//2 - TILE_SIZE//2
+	player_sprite = BasicSprite(starting_position=pygame.Vector2(player_x, player_y), flippable=True)
+	camera = SimpleCamera(width=VIEWPORT_WIDTH, height=VIEWPORT_HEIGHT, target=player_sprite)
+	world.add_component(player, Velocity())
+	world.add_component(player, player_sprite)
+	world.add_component(player, PlayerController())
+	world.add_component(player, camera)
+	world.add_component(player, Target(current=crosshair))
+	# Player weapons
+	weapon_left = world.create_entity()
+	weapon_left_sprite = BasicSprite(starting_position=pygame.Vector2(player_x - 20, player_y), file_name="weap_hand_L.png", flipped=True)
+	world.add_component(weapon_left, Velocity())
+	world.add_component(weapon_left, weapon_left_sprite)
+	world.add_component(weapon_left, Equippable(parent=player, original_image=weapon_left_sprite.image, offset_x=-20))
+	world.add_component(weapon_left, Target(current=crosshair))
+	weapon_right = world.create_entity()
+	weapon_right_sprite = BasicSprite(starting_position=pygame.Vector2(player_x + 20, player_y), file_name="weap_hand_L.png")
+	world.add_component(weapon_right, Velocity())
+	world.add_component(weapon_right, weapon_right_sprite)
+	world.add_component(weapon_right, Equippable(parent=player, original_image=weapon_right_sprite.image, offset_x=20))
+	world.add_component(weapon_right, Target(current=crosshair))
+	# Add systems to world
+	world.add_processor(RenderSystem(canvas=canvas, background=background, screen=viewport, camera=camera))
+	world.add_processor(InputSystem())
+	world.add_processor(MovementSystem(min_x=0, max_x=WORLD_WIDTH, min_y=0, max_y=WORLD_HEIGHT))
+	world.add_processor(CrosshairSystem(crosshair=crosshair, camera_target=player))
+	world.add_processor(WeaponSystem())
+	world.add_processor(ViewportSystem(max_width=WORLD_WIDTH, max_height=WORLD_HEIGHT))
+	# Return level instance (ECS world)
+	return world
+
+
 # -------------------------------------------------------------------------------------------------
 
 
 def run():
+	# Initialize pygame
 	pygame.init()
 	pygame.mouse.set_visible(False)
-	viewport = pygame.display.set_mode(SCREEN_SIZE.size)
-	pygame.display.set_caption("ECS CONVERSION")
-
+	# Create a clock instance (FPS limit)
 	clock = pygame.time.Clock()
-
-	background = pygame.Surface((WORLD_WIDTH, WORLD_HEIGHT)).convert()
-	canvas = pygame.Surface((WORLD_WIDTH, WORLD_HEIGHT)).convert()
-
-	tilemap = Tilemap(size=(MAP_WIDTH, MAP_HEIGHT), file_name=LEVEL_ARENA)
-	tilemap.render(background, False)
-
-	world = esper.World()
-
-	player = world.create_entity()
-	world.add_component(player, Velocity())
-	world.add_component(player, Renderable(starting_position=pygame.Vector2(WORLD_WIDTH/2, WORLD_HEIGHT/2)))
-	world.add_component(player, PlayerController())
-	world.add_component(player, SimpleCamera(width=VIEWPORT_WIDTH, height=VIEWPORT_HEIGHT, target=world.component_for_entity(player, Renderable)))
-
-	crosshair = world.create_entity()
-	world.add_component(crosshair, Renderable(file_name="cursor_crosshair.png", flippable=False))
-
-	world.add_processor(RenderSystem(canvas=canvas, background=background, viewport=viewport, camera=world.component_for_entity(player, SimpleCamera)))
-	world.add_processor(InputSystem())
-	world.add_processor(MovementSystem(min_x=0, max_x=WORLD_WIDTH, min_y=0, max_y=WORLD_HEIGHT))
-	world.add_processor(CrosshairSystem(crosshair=crosshair, camera_target=player))
-	world.add_processor(ViewportSystem(max_width=WORLD_WIDTH, max_height=WORLD_HEIGHT))
-
+	# Time passed since last tick
 	dt = 0
+
+	# Generate level and get related world instance
+	world = create_level_arena()
+
+	events = None
 	while 1:
-		# Handle general events
+		# General events handler
 		events = pygame.event.get()
 		for event in events:
 			if event.type == QUIT:
